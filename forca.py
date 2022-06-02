78568f7c92def98b3cba54abbe17e9d2c48fcb85
unknown <marcotuliodvaz@gamil.com>
2022-06-02 19:59:47 -0300

    Vresão inicail

---
+++ forca.py
@@ -0,0 +1,12 @@
+import pygame
+
+palavra_passe = []
+escolha_da_palavra = input('Digite sua palavra chave: ')
+dica_um = input('Digitea primeira dica: ')
+dica_dois = input('Digitea segunda dica: ')
+dica_treis = input('Digitea terceira dica: ') 
+
+palavra_passe.append(escolha_da_palavra)
+
+while True:
+    print()
