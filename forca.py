7e54e77e3c8afbb72bc588c7d6350cdc6fbd5f0f
Emily (Marco) <marcotuliodvaz@gamil.com>
2022-06-03 09:03:07 -0300

    palavra passe em *

--- forca.py
+++ forca.py
@@ -1,4 +1,6 @@
-import pygame
+from itertools import count
+from unicodedata import numeric
+
 
 palavra_passe = []
 escolha_da_palavra = input('Digite sua palavra chave: ')
@@ -8,5 +10,9 @@ dica_treis = input('Digitea terceira dica: ')
 
 palavra_passe.append(escolha_da_palavra)
 
+
 while True:
-    print('teste')
+    for range in palavra_passe:
+        numero_asterisco = len(palavra_passe[0]) * '*' 
+        print(numero_asterisco)
+
