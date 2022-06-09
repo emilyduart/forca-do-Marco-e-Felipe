97b3849726ee4df497a2bca50940c1c1fc72a4eb
Emily (Marco) <marcotuliodvaz@gamil.com>
2022-06-08 23:55:20 -0300

    Erros e acertos

--- forca.py
+++ forca.py
@@ -3,7 +3,7 @@ from unicodedata import numeric
 import os
 
 
-escolha_da_palavra = input('Digite sua palavra chave: ')
+escolha_da_palavra = input('Digite sua palavra chave com um espaço: ')
 dica_um = input('Digitea primeira dica: ')
 dica_dois = input('Digitea segunda dica: ')
 dica_treis = input('Digitea terceira dica: ') 
@@ -11,6 +11,9 @@ dica_treis = input('Digitea terceira dica: ')
 
 
 dica = -1
+erros = -1
+acertos = 0
+letras = []
 
 os.system ('cls')
 
@@ -22,7 +25,8 @@ while True:
     print('(2) veja uma dica.')
     opicao = int(input('Escolha uma opição: '))
     if opicao == 1:
-        letra_ecolhilda = input('Escolha uma letra: ')
+        letra_escolhilda = input('Escolha uma letra: ')
+        letras.append(letra_escolhilda)
     elif opicao == 2:
         dica += 1
         if dica == 0:
@@ -33,10 +37,18 @@ while True:
             print(dica_treis)
         else:
             print('Acabou as dicas!')
-        letra_ecolhilda = input('Escolha uma letra: ')
+        letra_escolhilda = input('Escolha uma letra: ')
+        letras.append(letra_escolhilda)
     else:
         print('Opição invalida!')
         break
-
-    if escolha_da_palavra.index(letra_ecolhilda) == True:
+    print(letras)
+    if letras[-1] in escolha_da_palavra:
         print('Letra certa!')
+    else:
+        print('Letra errada!')
+        erros += 1
+        if erros == 5:
+            print('Você perdeu!') 
+            input()
+            break
